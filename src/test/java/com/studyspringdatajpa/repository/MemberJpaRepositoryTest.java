99c5483961f8d1f6fbf81ab2a4d658ccf8b465d1
jkchappydev <jkchappydev@gmail.com>
2025-06-22 17:57:28 +0900

    study: section4 - 공통 인터페이스 적용

--- src/test/java/com/studyspringdatajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/com/studyspringdatajpa/repository/MemberJpaRepositoryTest.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
+// 순수 JPA 기반 테스트
 @SpringBootTest
 @Transactional // JPA는 트랜잭션 단위로 실행되므로 해당 어노테이션 반드시 필요함.
 @Rollback(false) // SpringBootTest에서는 기본적으로 트랜잭션 끝나고 rollback을 시킨다. 따라서 DB에는 아무것도 없는 것이 정상임. rollback을 시키지 않으려면 해당 어노테이션 false로 작성
